a9ec1d60eed8c66c02eef54691b39ac4e681f2e8
Riccardo Binetti <rbino@gmx.com>
2017-12-10 10:32:38 +0100

    Add jsfiddle POC

--- pexel/assets/js/app.js
+++ pexel/assets/js/app.js
@@ -12,6 +12,7 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
+import "jquery"
 
 // Import local files
 //
@@ -19,3 +20,59 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+function fillCanvas() {
+		var arr32 = new Uint32Array(10000)
+    arr32.fill(0xFF00FF00)
+		var arr = new Uint8ClampedArray(arr32.buffer)
+    var c = document.getElementById("main-canvas");
+    var ctx = c.getContext("2d");
+    ctx.imageSmoothingEnabled = false;
+    var imgData = new ImageData(arr, 100, 100)
+    ctx.putImageData(imgData, 0, 0);
+}
+
+function findPos(obj) {
+    var curleft = 0, curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curleft += obj.offsetLeft;
+            curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+        return { x: curleft, y: curtop };
+    }
+    return undefined;
+}
+
+function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255)
+        throw "Invalid color component";
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
+
+$('#fill-button').click(fillCanvas);
+
+$('#main-canvas').mousemove(function(e) {
+    var pos = findPos(this);
+    var x = Math.floor((e.pageX - pos.x) / 5);
+    var y = Math.floor((e.pageY - pos.y) / 5);
+    var coord = "x=" + x + ", y=" + y;
+    var c = this.getContext('2d');
+    var p = c.getImageData(x, y, 1, 1).data;
+    var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+    $('#status').html(coord + "<br>" + hex);
+});
+
+$('#main-canvas').click(function(e) {
+		var pos = findPos(this);
+    var x = Math.floor((e.pageX - pos.x) / 5);
+    var y = Math.floor((e.pageY - pos.y) / 5);
+    var c = document.getElementById("main-canvas");
+    var ctx = c.getContext("2d");
+    var id = ctx.createImageData(1,1);
+    var d  = id.data;
+    d[0] = 0xff;
+    d[1] = 0x00;
+    d[2] = 0x00;
+    d[3] = 0xff;
+    ctx.putImageData(id, x, y);
+})
