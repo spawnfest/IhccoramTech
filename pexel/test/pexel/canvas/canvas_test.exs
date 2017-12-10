bf9e7135d99629ec55cc87110718a600ca20e941
Riccardo Binetti <rbino@gmx.com>
2017-12-10 22:41:31 +0100

    Fix canvas_test

--- pexel/test/pexel/canvas/canvas_test.exs
+++ pexel/test/pexel/canvas/canvas_test.exs
@@ -6,9 +6,9 @@ defmodule Pexel.CanvasTest do
   describe "tiles" do
     alias Pexel.Canvas.Tile
 
-    @valid_attrs %{color: "some color", x: 42, y: 42}
-    @update_attrs %{color: "some updated color", x: 43, y: 43}
-    @invalid_attrs %{color: nil, x: nil, y: nil}
+    @valid_attrs %{"color" => 1, "x" => 42, "y" => 42}
+    @update_attrs %{"color" => 2, "x" => 42, "y" => 42}
+    @invalid_attrs %{"color" => 18, "x" => 42, "y" => 42}
 
     def tile_fixture(attrs \\ %{}) do
       {:ok, tile} =
@@ -26,12 +26,12 @@ defmodule Pexel.CanvasTest do
 
     test "get_tile!/1 returns the tile with given id" do
       tile = tile_fixture()
-      assert Canvas.get_tile!(tile.id) == tile
+      assert Canvas.get_tile!(tile.x, tile.y) == tile
     end
 
     test "create_tile/1 with valid data creates a tile" do
       assert {:ok, %Tile{} = tile} = Canvas.create_tile(@valid_attrs)
-      assert tile.color == "some color"
+      assert tile.color == 1
       assert tile.x == 42
       assert tile.y == 42
     end
@@ -44,21 +44,21 @@ defmodule Pexel.CanvasTest do
       tile = tile_fixture()
       assert {:ok, tile} = Canvas.update_tile(tile, @update_attrs)
       assert %Tile{} = tile
-      assert tile.color == "some updated color"
-      assert tile.x == 43
-      assert tile.y == 43
+      assert tile.color == 2
+      assert tile.x == 42
+      assert tile.y == 42
     end
 
     test "update_tile/2 with invalid data returns error changeset" do
       tile = tile_fixture()
       assert {:error, %Ecto.Changeset{}} = Canvas.update_tile(tile, @invalid_attrs)
-      assert tile == Canvas.get_tile!(tile.id)
+      assert tile == Canvas.get_tile!(tile.x, tile.y)
     end
 
     test "delete_tile/1 deletes the tile" do
       tile = tile_fixture()
       assert {:ok, %Tile{}} = Canvas.delete_tile(tile)
-      assert_raise Ecto.NoResultsError, fn -> Canvas.get_tile!(tile.id) end
+      assert_raise Ecto.NoResultsError, fn -> Canvas.get_tile!(tile.x, tile.y) end
     end
 
     test "change_tile/1 returns a tile changeset" do
