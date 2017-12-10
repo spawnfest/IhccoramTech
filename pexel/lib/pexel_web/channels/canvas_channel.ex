3f52dbb7648d86333771e6c1ac371ea962af134f
Riccardo Binetti <rbino@gmx.com>
2017-12-10 23:38:23 +0100

    canvas_channel: add broadcast_new_tile function

--- pexel/lib/pexel_web/channels/canvas_channel.ex
+++ pexel/lib/pexel_web/channels/canvas_channel.ex
@@ -1,7 +1,15 @@
 defmodule PexelWeb.CanvasChannel do
   use PexelWeb, :channel
 
+  alias Pexel.Canvas.Tile
+
   def join("canvas:updates", _payload, socket) do
     {:ok, socket}
+  end
+
+  def broadcast_new_tile(%Tile{x: x, y: y, color: color}) do
+    payload = %{x: x, y: y, color: color}
+
+    PexelWeb.Endpoint.broadcast("canvas:updates", "new_tile", payload)
   end
 end
