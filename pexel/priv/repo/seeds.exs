81a2391a000b3dec69025ff7a8a7ec76be2f23db
Riccardo Binetti <rbino@gmx.com>
2017-12-10 16:49:43 +0100

    Add basic tile seeds

--- pexel/priv/repo/seeds.exs
+++ pexel/priv/repo/seeds.exs
@@ -9,3 +9,27 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+defmodule Pexel.DBSeeder do
+  alias Pexel.Repo
+  alias Pexel.Canvas.Tile
+
+  def insert_black_tile(x, y) do
+    Repo.insert!(%Tile{
+      x: x,
+      y: y,
+      color: 0
+    })
+  end
+
+  def clear do
+    Repo.delete_all(Tile)
+  end
+end
+
+Pexel.DBSeeder.clear()
+
+for x <- 0..99, y <- 0..99 do
+  {x, y}
+end
+|> Enum.each(fn {x, y} -> Pexel.DBSeeder.insert_black_tile(x, y) end)
