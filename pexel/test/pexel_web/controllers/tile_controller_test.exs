834bca9113be045afc1d0e1705255b137764179a
Riccardo Binetti <rbino@gmx.com>
2017-12-10 22:49:47 +0100

    Fix controller tests

--- pexel/test/pexel_web/controllers/tile_controller_test.exs
+++ pexel/test/pexel_web/controllers/tile_controller_test.exs
@@ -4,9 +4,9 @@ defmodule PexelWeb.TileControllerTest do
   alias Pexel.Canvas
   alias Pexel.Canvas.Tile
 
-  @create_attrs %{color: "some color", x: 42, y: 42}
-  @update_attrs %{color: "some updated color", x: 43, y: 43}
-  @invalid_attrs %{color: nil, x: nil, y: nil}
+  @create_attrs %{color: 10, x: 42, y: 42}
+  @update_attrs %{color: 11, x: 42, y: 42}
+  @invalid_attrs %{color: 20, x: 42, y: 42}
 
   def fixture(:tile) do
     {:ok, tile} = Canvas.create_tile(@create_attrs)
@@ -27,12 +27,11 @@ defmodule PexelWeb.TileControllerTest do
   describe "create tile" do
     test "renders tile when data is valid", %{conn: conn} do
       conn = post conn, tile_path(conn, :create), tile: @create_attrs
-      assert %{"id" => id} = json_response(conn, 201)["data"]
+      assert %{"x" => x, "y" => y} = json_response(conn, 201)["data"]
 
-      conn = get conn, tile_path(conn, :show, id)
+      conn = get conn, tile_path(conn, :show, x, y)
       assert json_response(conn, 200)["data"] == %{
-        "id" => id,
-        "color" => "some color",
+        "color" => 10,
         "x" => 42,
         "y" => 42}
     end
@@ -46,33 +45,20 @@ defmodule PexelWeb.TileControllerTest do
   describe "update tile" do
     setup [:create_tile]
 
-    test "renders tile when data is valid", %{conn: conn, tile: %Tile{id: id} = tile} do
-      conn = put conn, tile_path(conn, :update, tile), tile: @update_attrs
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
+    test "renders tile when data is valid", %{conn: conn, tile: %Tile{x: x, y: y}} do
+      conn = put conn, tile_path(conn, :update, x, y), tile: @update_attrs
+      assert %{"x" => ^x, "y" => ^y} = json_response(conn, 200)["data"]
 
-      conn = get conn, tile_path(conn, :show, id)
+      conn = get conn, tile_path(conn, :show, x, y)
       assert json_response(conn, 200)["data"] == %{
-        "id" => id,
-        "color" => "some updated color",
-        "x" => 43,
-        "y" => 43}
+        "color" => 11,
+        "x" => 42,
+        "y" => 42}
     end
 
-    test "renders errors when data is invalid", %{conn: conn, tile: tile} do
-      conn = put conn, tile_path(conn, :update, tile), tile: @invalid_attrs
+    test "renders errors when data is invalid", %{conn: conn, tile: %Tile{x: x, y: y}} do
+      conn = put conn, tile_path(conn, :update, x, y), tile: @invalid_attrs
       assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
-
-  describe "delete tile" do
-    setup [:create_tile]
-
-    test "deletes chosen tile", %{conn: conn, tile: tile} do
-      conn = delete conn, tile_path(conn, :delete, tile)
-      assert response(conn, 204)
-      assert_error_sent 404, fn ->
-        get conn, tile_path(conn, :show, tile)
-      end
     end
   end
 
